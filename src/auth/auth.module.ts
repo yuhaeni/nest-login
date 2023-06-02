4cdf7a8c06efc0146e9fbceb8fd5e121ce74ff5d
haeni311 <haeni311@gmail.com>
2023-06-02 17:14:52 +0900

    JwtModeule과 JwtStrategy를 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,23 @@ import { UserRepository } from 'src/repository/user.repository';
 import { LocalStrategy } from './strategies/auth.local.strategy';
 import { AuthService } from './auth.service';
 import { TypeOrmExModule } from 'src/db/typeorm-ex.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategies/auth.jwt.strategy';
 
 @Module({
-    imports: [TypeOrmExModule.forCustomRepository([UserRepository]) ,PassportModule],
-    providers: [AuthService, LocalStrategy], 
+    imports: [
+        TypeOrmExModule.forCustomRepository([UserRepository]) ,
+        PassportModule,
+        JwtModule.register({
+            // 토큰 서명 값 설정
+            secret: jwtConstants.secret ,
+            // 토큰 유효시간 (임의 60초)
+            signOptions: {expiresIn: '60s'},
+        }),
+        TypeOrmModule.forFeature([UserRepository]),
+    ],
+    providers: [AuthService, LocalStrategy ,JwtStrategy], 
+    exports: [AuthService],
 })
 export class AuthModule {}
